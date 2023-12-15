53a46f1785b29a9e03df68561dc941126ce9853d
SagarDhyani <sagardhyani0@gmail.com>
2023-12-16 02:14:12 +0530

    add project initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>knazlamlm</div>}></Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const router = require("./router");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("a user connected")
+
+    socket.on('disconnect', () => {
+        console.log("user has left")
+    })
+})
+
+app.use("/", router);
+
+server.listen(PORT, () => console.log(`server running on ${PORT} port`));
